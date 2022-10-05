5e735e289fd40167bbd01a73f73199939df6a428
fredyxander <freddy5210@gmail.com>
2022-10-04 19:16:11 -0500

    clase1

---
+++ Clase1/variables.js
@@ -0,0 +1,36 @@
+// console.log("hola mundo");
+
+//variables
+// var texto = "texto";
+// var numero = 981274;
+// var isValid = true;
+
+// var texto = 728634;
+
+// console.log(texto);
+
+let variable1 = "texto";
+variable1 = 87623; //reasignar
+console.log(variable1);
+
+const year = 2022;
+// year = 2010;
+console.log(year);
+
+// if(true){
+//     var variableA ="curso nodejs";
+//     console.log(variableA);
+// }
+// console.log(variableA);
+
+// if(true){
+//     let variableA ="curso nodejs";
+//     console.log(variableA);
+// }
+
+//mutabilidad
+const numeros = [1,2,3,4];
+// numeros="otro dato";//no puedo reasignar con const
+numeros[1] = "dato";//mutabilidad
+console.log(numeros);
+
